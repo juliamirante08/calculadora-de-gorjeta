58db8cc27dc29bb3edf50d75d3f7221370da3ad5
julia mirante <julia.mirante@eaportal.org>
2024-03-27 22:18:48 -0300

    quinto commit do JS e projeto finalizado

--- script.js
+++ script.js
@@ -48,9 +48,36 @@ function receberPorcentagem(evento) {
     }
 
    
-    console.log(porcentagem)
+   calcular()
 } 
 
 const gorjetaInput = document.querySelector("#outra")
 gorjetaInput.addEventListener("input",receberPorcentagem)
 
+function calcular() {
+    if(conta !== 0 && porcentagem !== 0 && pessoas !== 0) {
+       const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
+       strongGorjetaTotal.innerHTML = `R$ ${(conta * gorjeta / pessoas).toFixed(2)}`
+
+       const strongTotal = document.querySelector(".total > strong")
+       strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas ).toFixed(2)}`
+    }
+}
+
+const botaoLimpar = document.querySelector(".resultados button")
+botaoLimpar.addEventListener("click", limpar)
+
+function limpar() {
+    contaInput.value = ""
+
+    botoesGorjeta.forEach(botao => {
+        botao.classList.remove("botao-ativo")
+    })
+
+    gorjetaInput.value = ""
+
+    pessoasInput.value = ""
+
+    document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
+    document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+}
